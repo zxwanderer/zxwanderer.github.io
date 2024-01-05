7f7aa889745a869520d57e60b8d69c5b6e44261d
zxwanderer <no@mail.com>
2024-01-05 12:00:13 +0300

    add some useful file

---
+++ git-set-local.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git config --local user.name "zxwanderer"
+git config --local user.email "no@mail.com"
+git config --local core.sshCommand 'ssh -i ~/.ssh/ed_wanderer -o IdentityAgent=none'
